b0bf1ded81b7972fbefcadaec63252e7b7924424
ItsCraftIsBored <athaya@itscraftsoftware.my.id>
2022-01-10 07:49:01 +0700

    Add files via upload

---
+++ fnaf3main.py
@@ -0,0 +1,38 @@
+print("system restart")
+print("menu >>>")
+print("")
+print("audio devices")
+print("camera system")
+print("ventilation")
+print("")
+print("reboot all")
+print("exit")
+print("")
+print(">")
+sys = input()
+
+if sys ==  "audio devices":
+    print("............ Complete !")
+    
+if sys == "camera system":
+    print("............ Complete !")
+    
+if sys == "ventilation":
+    print("............ Complete !")
+    
+if sys == "reboot all":
+    print("............ Complete !")
+    
+if sys == "exit":
+    print("")
+    
+if sys == "audio": 
+    print("............ Complete !")
+    
+if sys == "camera":
+    print("............ Complete !")
+    
+if sys == "reboot":
+    print("............ Complete !")
+
+input('')    
